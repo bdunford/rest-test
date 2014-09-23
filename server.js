12d607dd49054ad72088fc9654faaf56db2cf571
Birch Dunford <birch.dunford@caresource.com>
2014-09-23 14:40:53 -0400

    Changed order for readability. Added check for process.env.PORT

--- server.js
+++ server.js
@@ -22,21 +22,18 @@ function getResponseBody(req) {
 
 var app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
+app.set('port', (process.env.PORT || 3001))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.listen(3001);
-
 app.all('/status/:status*', function(req, res){
+    console.log(req.params.status)
     log(req);
     if (!req.params.status || req.params.status == 200) {
         res.send(getResponseBody(req));
     } else {
         res.statusCode = req.params.status
-        res.send({});
+        res.send();
     }
 });
 
@@ -45,4 +42,7 @@ app.all('/*',function(req,res) {
     res.send(getResponseBody(req));
 });
 
-console.log('Listening on port 3001...');
+
+app.listen(app.get('port'), function() {
+  console.log("Listening on port " + app.get('port') + "...");
+})
